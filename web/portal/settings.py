cb6985223f5fb8aa90e742e3d285209109740d21
Fireblade <moalexv@mail.ru>
2018-12-27 21:55:41 +0300

    Willzscoring-46 part1

--- web/portal/settings.py
+++ web/portal/settings.py
@@ -66,7 +66,7 @@ ROOT_URLCONF = 'web.portal.urls'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': 'mydatabase',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
