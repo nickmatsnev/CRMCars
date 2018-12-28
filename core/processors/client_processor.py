330153c4c1e6fb7c68728940de6a738b6202091a
Fireblade <moalexv@mail.ru>
2018-12-28 17:03:43 +0300

    Willzscoring-46 part2 - complete. Update requests

--- core/processors/client_processor.py
+++ core/processors/client_processor.py
@@ -5,8 +5,8 @@ sys.path.append('../')
 
 from lib.process import *
 from lib.constants import *
-from rest_framework.test import APIRequestFactory
 
+import requests
 import json
 
 class ClientProcessor(BasicProcess):
@@ -18,12 +18,13 @@ class ClientProcessor(BasicProcess):
                              })
 
     def __process_raw_client(self, body):
-        factory = APIRequestFactory()
+        url = "http://127.0.0.1:8000"
+        headers = {"accept": "application/json",  "X-CSRFToken": "hDnSpAaGh1PBYRa4mLozhjdXUMKXVIeOsYXUbJpcGtGoJ5KxZKF9kmjpZj3hKJGD"}
 
         #дергаем сырок
         input_message = json.loads(body)
         raw_client_id = input_message['raw_client_id']
-        response = factory.get('/api/willz/{0}'.format(raw_client_id))
+        response = requests.request(method='GET', url=url+'/api/willz/{0}'.format(raw_client_id), headers=headers)
 
         #парсим полученное
         raw_data = json.loads(response)
@@ -31,7 +32,7 @@ class ClientProcessor(BasicProcess):
 
         #делаем формат для Клиента, отправляем и получаем client_id
         raw_to_client = json.load({'willz_id': raw_json['id'],'created_at': raw_json['created_at']})
-        response = factory.post('/api/clients',json.dumps(raw_to_client), content_type='application/json')
+        response = requests.request(method='POST', url=url + '/api/clients',data=json.dumps(raw_to_client), content_type='application/json', headers=headers)
         raw_data = json.loads(response)
         client_id = raw_data['id']
 
@@ -42,16 +43,17 @@ class ClientProcessor(BasicProcess):
                 raw_to_individual = json.load({'client': client_id,'last_name': drvr['lastname']
                                                   , 'first_name': drvr['firstname'],'middle_name': drvr['middlename'],'email': drvr['email']
                                                   , 'phone': drvr['phone'],'gender': drvr['gender_id'],'birthday': drvr['birthday']})
-                response = factory.post('/api/individuals', json.dumps(raw_to_individual),
-                             content_type='application/json')
+                response = requests.request(method='POST', url=url + '/api/individuals', data=json.dumps(raw_to_individual),
+                                            content_type='application/json', headers=headers)
                 raw_data = json.loads(response)
                 individual_id = raw_data['id']
 
                 # формируем права
                 raw_to_driving_license = json.load({'individual': individual_id,'number': drvr['driver_license']['number']
                                                   , 'issued_at': drvr['driver_license']['issued_at']})
-                response = factory.post('/api/driver_licenses', json.dumps(raw_to_driving_license),
-                                        content_type='application/json')
+                response = requests.request(method='POST', url=url + '/api/driver_licenses',
+                                            data=json.dumps(raw_to_driving_license),
+                                            content_type='application/json', headers=headers)
                 raw_data = json.loads(response)
                 driver_license_id = raw_data['id']
 
@@ -60,8 +62,9 @@ class ClientProcessor(BasicProcess):
                     new_img_txt = 'image{0}'.format(img)
                     raw_to_img = json.load({'individual': individual_id,'driver_license': driver_license_id,
                                             'title': drvr['passport'][new_img_txt],'url': drvr['passport'][new_img_txt + '_url']})
-                    factory.post('/api/images', json.dumps(raw_to_img),
-                                 content_type='application/json')
+                    response = requests.request(method='POST', url=url + '/api/images',
+                                                data=json.dumps(raw_to_img),
+                                                content_type='application/json', headers=headers)
 
                 # формируем паспорт
                 raw_to_passport = json.load(
@@ -69,8 +72,9 @@ class ClientProcessor(BasicProcess):
                         , 'issued_at': drvr['passport']['issued_at'], 'issued_by': drvr['passport']['issued_by']
                         , 'address_registration': drvr['passport']['address_registration']
                         , 'division_code': drvr['passport']['division_code'], 'birthplace': drvr['passport']['birthplace']})
-                response = factory.post('/api/driver_licenses', json.dumps(raw_to_driving_license),
-                                        content_type='application/json')
+                response = requests.request(method='POST', url=url + '/api/driver_licenses',
+                                            data=json.dumps(raw_to_driving_license),
+                                            content_type='application/json', headers=headers)
                 raw_data = json.loads(response)
                 passport_id = raw_data['id']
 
@@ -79,8 +83,9 @@ class ClientProcessor(BasicProcess):
                     new_img_txt = 'image{0}'.format(img)
                     raw_to_img = json.load({'individual': individual_id,'passport': passport_id,
                                             'title': drvr['passport'][new_img_txt],'url': drvr['passport'][new_img_txt + '_url']})
-                    factory.post('/api/images', json.dumps(raw_to_img),
-                                 content_type='application/json')
+                    response = requests.request(method='POST', url=url + '/api/images',
+                                                data=json.dumps(raw_to_img),
+                                                content_type='application/json', headers=headers)
                 break
 
         #для всех индивидуалов
@@ -92,8 +97,9 @@ class ClientProcessor(BasicProcess):
                                                'email': drvr['email']
                                                   , 'phone': drvr['phone'], 'gender': drvr['gender_id'],
                                                'birthday': drvr['birthday']})
-                response = factory.post('/api/individuals', json.dumps(raw_to_individual),
-                                        content_type='application/json')
+                response = requests.request(method='POST', url=url + '/api/individuals',
+                                            data=json.dumps(raw_to_individual),
+                                            content_type='application/json', headers=headers)
                 raw_data = json.loads(response)
                 individual_id = raw_data['id']
 
@@ -101,8 +107,9 @@ class ClientProcessor(BasicProcess):
                 raw_to_driving_license = json.load(
                     {'individual': individual_id, 'number': drvr['driver_license']['number']
                         , 'issued_at': drvr['driver_license']['issued_at']})
-                response = factory.post('/api/driver_licenses', json.dumps(raw_to_driving_license),
-                                        content_type='application/json')
+                response = requests.request(method='POST', url=url + '/api/driver_licenses',
+                                            data=json.dumps(raw_to_driving_license),
+                                            content_type='application/json', headers=headers)
                 raw_data = json.loads(response)
                 driver_license_id = raw_data['id']
 
@@ -112,8 +119,9 @@ class ClientProcessor(BasicProcess):
                     raw_to_img = json.load({'individual': individual_id, 'driver_license': driver_license_id,
                                             'title': drvr['passport'][new_img_txt],
                                             'url': drvr['passport'][new_img_txt + '_url']})
-                    factory.post('/api/images', json.dumps(raw_to_img),
-                                 content_type='application/json')
+                    response = requests.request(method='POST', url=url + '/api/images',
+                                                data=json.dumps(raw_to_img),
+                                                content_type='application/json', headers=headers)
 
                 # формируем паспорт
                 raw_to_passport = json.load(
@@ -122,8 +130,9 @@ class ClientProcessor(BasicProcess):
                         , 'address_registration': drvr['passport']['address_registration']
                         , 'division_code': drvr['passport']['division_code'],
                      'birthplace': drvr['passport']['birthplace']})
-                response = factory.post('/api/driver_licenses', json.dumps(raw_to_driving_license),
-                                        content_type='application/json')
+                response = requests.request(method='POST', url=url + '/api/driver_licenses',
+                                            data=json.dumps(raw_to_driving_license),
+                                            content_type='application/json', headers=headers)
                 raw_data = json.loads(response)
                 passport_id = raw_data['id']
 
@@ -133,13 +142,11 @@ class ClientProcessor(BasicProcess):
                     raw_to_img = json.load({'individual': individual_id, 'passport': passport_id,
                                             'title': drvr['passport'][new_img_txt],
                                             'url': drvr['passport'][new_img_txt + '_url']})
-                    factory.post('/api/images', json.dumps(raw_to_img),
-                                 content_type='application/json')
-
+                    response = requests.request(method='POST', url=url + '/api/images',
+                                                data=json.dumps(raw_to_img),
+                                                content_type='application/json', headers=headers)
         print("client is processed")
         self._publish_message(CLIENT_PROCESSED_MESSAGE,"test_body")
-
-
 
 
 proc = ClientProcessor()
