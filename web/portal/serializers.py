58130fbb75358372086a98c9ad97e3e5750b7ae2
Fireblade <moalexv@mail.ru>
2019-01-18 01:23:44 +0300

    Generation tasks

--- web/portal/serializers.py
+++ web/portal/serializers.py
@@ -1,6 +1,6 @@
 from rest_framework import serializers
 from web.portal.models import *
-
+import datetime
 
 class CheckModelSerializer(serializers.ModelSerializer):
     class Meta:
@@ -74,34 +74,84 @@ class TaskSerializer(serializers.ModelSerializer):
 
 
 class ClientTaskSerializer(serializers.ModelSerializer):
+    task = TaskSerializer(many=False)
+    raw_client_data = RawClientDataSerializer(many=False)
+    client = ClientSerializer(many=False)
+
     class Meta:
         model = ClientTask
-        fields = ('raw_client_data', 'client')
+        # таск надо спрятать
+        fields = ('raw_client_data', 'client', 'task')
 
-  #  def create(self, validated_data):
-  #      task = TaskSerializer
-  #       status = in_progress...
+    def create(self, validated_data):
+        raw_client_data_data = validated_data.pop('raw_client_data')
+        client_data = validated_data.pop('client')
+
+        client_task = ClientTask.objects.create(**validated_data)
+        new_task = Task.objects.create()
+
+        new_task.create_time = datetime.datetime.now()
+        new_task.status = "in_progress"
+        new_task.task_type = 0
+        client_task.task = new_task.save()
+
+        RawClientData.objects.create(client_task=client_task, **raw_client_data_data)
+        Client.objects.create(client_task=client_task, **client_data)
+        return client_task
 
 
-class CreateClientTaskSerializer(ClientTaskSerializer):
+class SourceTaskSerializer(serializers.ModelSerializer):
     task = TaskSerializer(many=False)
-    raw_client_data = RawClientDataSerializer(many=False)
-    client = ClientSerializer(many=False)
+    source_raw_data = SourceRawDataSerializer(many=False)
+    individual = IndividualSerializer(many=False)
 
+    class Meta:
+        model = SourceTask
+        # таск надо спрятать
+        fields = ('source_raw_data', 'individual', 'task')
 
-class UpdateClientTaskSerializer(ClientTaskSerializer):
-    task = TaskSerializer(many=False)
-    raw_client_data = RawClientDataSerializer(many=False)
-    client = ClientSerializer(many=False)
+    def create(self, validated_data):
+        source_raw_data_data = validated_data.pop('source_raw_data')
+        individual_data = validated_data.pop('individual')
+
+        source_task = SourceTask.objects.create(**validated_data)
+        new_task = Task.objects.create()
+
+        new_task.create_time = datetime.datetime.now()
+        new_task.status = "in_progress"
+        new_task.task_type = 1
+        source_task.task = new_task.save()
+
+        SourceRawData.objects.create(source_task=source_task, **source_raw_data_data)
+        Individual.objects.create(source_task=source_task, **individual_data)
+        return source_task
 
 
 class GenerationCreateSerializer(ClientSerializer):
+    source_task = ClientTaskSerializer()
+    individual = IndividualSerializer()
+
     class Meta:
         model = Generation
         fields = ('individual','source_task')
+
+    def create(self, validated_data):
+        source_task_data = validated_data.pop('source_task')
+        individual_data = validated_data.pop('individual')
+        generation = Generation.objects.create(**validated_data)
+        generation.create_time = datetime.datetime.now()
+# проверка на наличие генераций, если нет -, если есть то +1
+#       if (Generation.objects.get(individual=individual)!=0:
+            #+1
+        #else
+        generation.number=0
+        SourceTask.objects.create(generation=generation, **source_task_data)
+        Individual.objects.create(generation=generation, **individual_data)
+        return generation
 
 
 class GenerationGetSerializer(ClientSerializer):
     class Meta:
         model = Generation
         fields = ('id', 'individual', 'number', 'create_time', 'source_task')
+
