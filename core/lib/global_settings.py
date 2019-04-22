12de9c829ceef1831edaa2170bf2d6f674900cd6
FFFireblade <moalexv@mail.ru>
2019-04-22 19:44:48 +0300

    added Excel to requests formatter (not ready for 100%!)

--- core/lib/global_settings.py
+++ core/lib/global_settings.py
@@ -4,3 +4,6 @@ API_ROOT_URL = "http://127.0.0.1:8002/api"
 EXCEL_COLUMN_WIDTH_MAX = 100
 EXCEL_COLUMN_WIDTH_BASE = 10
 
+EXCEL_TO_READ_PATH = "C:\\Users\\Developer_Web\\Downloads\\passport_data.xlsx"
+EXCEL_TO_READ_PASSPORT_PATH = "https://new.willz.ru/uploads/passports/"
+EXCEL_TO_READ_DRIVER_LICENSES_PATH = "https://new.willz.ru/uploads/driverlicenses/"
